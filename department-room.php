65b2aa4b61ddc198256ccc21cd2000e683340536
unknown <mirvatbarakat1@gmail.com>
2023-03-13 08:27:23 +0200

    Added a PHP API that will get departments and rooms available in that departments

---
+++ department-room.php
@@ -0,0 +1,23 @@
+<?php
+include('connection.php');
+
+$sql = "SELECT * FROM departments";
+$result = $mysqli>query($sql);
+$departments = array();
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $departments[] = $row;
+  }
+}
+
+$department_id = $_GET['department_id'];
+$sql1 = "SELECT * FROM rooms WHERE department_id = '$department_id'";
+$result = $mysqli->query($sql1);
+$rooms = array();
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $rooms[] = $row;
+  }
+}
+
+?>
